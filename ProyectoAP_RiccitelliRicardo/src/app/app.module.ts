cea0f11e05bec110e14a1ae2ff4fa124ef51c0e6
Rickys89 <r.riccitelli89@gmail.com>
2022-10-17 21:13:45 -0300

    Creación Bootstrap

--- ProyectoAP_RiccitelliRicardo/src/app/app.module.ts
+++ ProyectoAP_RiccitelliRicardo/src/app/app.module.ts
@@ -3,10 +3,20 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './components/header/header.component';
+import { EducacionComponent } from './components/educacion/educacion.component';
+import { LogoComponent } from './components/logo/logo.component';
+import { SocialComponent } from './components/social/social.component';
+import { BannerComponent } from './components/banner/banner.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    EducacionComponent,
+    LogoComponent,
+    SocialComponent,
+    BannerComponent
   ],
   imports: [
     BrowserModule,

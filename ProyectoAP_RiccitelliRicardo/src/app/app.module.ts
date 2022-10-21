ba9446d9c999f7b84c95f6211a4637578d295185
Rickys89 <r.riccitelli89@gmail.com>
2022-10-20 23:02:15 -0300

    Creacion de banner y acerca de

--- ProyectoAP_RiccitelliRicardo/src/app/app.module.ts
+++ ProyectoAP_RiccitelliRicardo/src/app/app.module.ts
@@ -8,6 +8,7 @@ import { EducacionComponent } from './components/educacion/educacion.component';
 import { LogoComponent } from './components/logo/logo.component';
 import { SocialComponent } from './components/social/social.component';
 import { BannerComponent } from './components/banner/banner.component';
+import { AcercaDeComponent } from './components/acerca-de/acerca-de.component';
 
 @NgModule({
   declarations: [
@@ -16,7 +17,8 @@ import { BannerComponent } from './components/banner/banner.component';
     EducacionComponent,
     LogoComponent,
     SocialComponent,
-    BannerComponent
+    BannerComponent,
+    AcercaDeComponent
   ],
   imports: [
     BrowserModule,
